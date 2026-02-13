01384a3e24c0da4eda0d9be9382dfe478d907b37
BAO <ghost@github.com>
2026-02-13 23:12:57 +0800

    build(build.sh): 移除pdflatex回退并更新错误消息
    - 移除对pdflatex的检查和支持，只保留xelatex作为编译工具
    - 更新错误消息以反映只有xelatex可用的情况
    
    refactor(images): 删除未使用的UN徽标下载脚本
    
    - 移除load_un_logo.sh脚本，该脚本用于下载联合国徽标
    - 该脚本不再被项目使用，因此将其删除以简化代码库

--- build.sh
+++ build.sh
@@ -7,12 +7,10 @@ if ! [ -d "$OUTPUTDIR" ]; then
   mkdir "$OUTPUTDIR"
 fi
 
-if command -v pdflatex >/dev/null 2>&1; then
-  pdflatex -interaction=nonstopmode -output-directory="$OUTPUTDIR" "$SOURCES"
-elif command -v xelatex >/dev/null 2>&1; then
+if command -v xelatex >/dev/null 2>&1; then
   xelatex -interaction=nonstopmode -output-directory="$OUTPUTDIR" "$SOURCES"
 else
-  echo "Neither pdflatex nor xelatex found. You need either of them to compile the document."
+  echo "No xelatex found. You need either of them to compile the document."
   exit 1
 fi
 
